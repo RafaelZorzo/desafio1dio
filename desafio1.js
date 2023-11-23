393d781910000d85e0f75d5cba67de7f925373a4
RafaelZorzo <rafael2zorzo@gmail.com>
2023-11-23 20:20:54 -0300

    desafio realizado

---
+++ desafio1.js
@@ -0,0 +1,35 @@
+//Elos do valorant cara kkkk :)
+let nome = "Defalt"
+let xp = 10001
+let nivel 
+if (xp <= 1000){
+    nivel = "Ferro"
+    console.log("O Herói de nome " + nome + " está no nível de " + nivel)
+} else if (xp > 1000 && xp <= 2000) {
+    nivel = "Bronze"
+    console.log("O Herói de nome " + nome + " está no nível de " + nivel)
+  }
+    else if (xp > 2000 && xp <= 5000){
+      nivel = "Prata"
+      console.log("O Herói de nome " + nome + " está no nível de " + nivel)
+    }
+      else if (xp > 5000 && xp <= 7000){
+        nivel = "Ouro"
+        console.log("O Herói de nome " + nome + " está no nível de " + nivel)
+      }
+        else if (xp > 7000 && xp <= 8000){
+          nivel = "Platina"
+          console.log("O Herói de nome " + nome + " está no nível de " + nivel)
+        }
+          else if (xp > 8000 && xp <= 9000){
+            nivel = "Ascendente"
+            console.log("O Herói de nome " + nome + " está no nível de " + nivel)
+          }
+            else if (xp > 9000 && xp <= 10000){
+              nivel = "Imortal"
+              console.log("O Herói de nome " + nome + " está no nível de " + nivel)
+            }
+              else if (xp > 10000){
+                nivel = "Radiante"
+                console.log("O Herói de nome " + nome + " está no nível de " + nivel)
+              }
